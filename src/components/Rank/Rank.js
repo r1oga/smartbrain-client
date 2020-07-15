2042d8c272d1d9b6b423c552b7795153a3af67f0
r1oga <r1oga@tuta.io>
2020-07-15 23:12:47 +0200

    feat: Display badges instead of entries count

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+
+const LAMBDA_URI =
+  'https://1lynhlrtql.execute-api.eu-central-1.amazonaws.com/prod/rank'
 
 const Rank = ({ name, entries }) => {
+  const [emoji, setEmoji] = useState('')
+
+  const generateEmoji = entries => {
+    fetch(`${LAMBDA_URI}?rank=${entries}`)
+      .then(res => res.json())
+      .then(data => setEmoji(data.input))
+      .catch(console.log)
+  }
+  useEffect(() => {
+    generateEmoji(entries)
+  }, [])
+
   return (
     <div>
       <div className='white f3'>
         {`${name}, your current entry count is...`}
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
+      <div className='white f3'>{`Rank badge: ${emoji}`}</div>
     </div>
-  );
+  )
 }
 
-export default Rank;
+export default Rank
