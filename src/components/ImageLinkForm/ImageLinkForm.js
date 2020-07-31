611f078d5591f646b34cafda4a9fe0667883ca88
Gauthier Riou <r1oga@tuta.io>
2020-07-31 09:17:32 +0200

    Update ImageLinkForm.js

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -13,6 +13,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
             className='f4 pa2 w-70 center'
             type='tex'
             onChange={onInputChange}
+            placeholder='Face image URL: http://...'
           />
           <button
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
